2714fe5d09946c40572adeba4d10ff98748cc958
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2014-09-14 03:43:11 +0900

    modify 04_upload-image-to-glance.sh

--- 04_upload-image-to-glance.sh
+++ 04_upload-image-to-glance.sh
@@ -1,4 +1,4 @@
-glance image-create --name nakajima-centos-base-test --is-public false \
+glance image-create --name centos-base --is-public false \
 --disk-format qcow2 --container-format bare \
 --file /tmp/centos6-base.qcow2 \
 --progress
