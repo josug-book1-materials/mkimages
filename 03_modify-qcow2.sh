d594bffda79699483dc205f8841638baafe5ab80
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2014-09-14 12:29:27 +0900

    Update 03_modify-qcow2.sh

--- 03_modify-qcow2.sh
+++ 03_modify-qcow2.sh
@@ -1,4 +1,4 @@
 virsh destroy centos6
 virt-sysprep -d centos6
 virsh undefine centos6
-qemu-img convert -c -O qcow2 /tmp/centos6.qcow2 /tmp/centos6-base.qcow2
+qemu-img convert -p -c -O qcow2 /tmp/centos6.qcow2 /tmp/centos6-base.qcow2
