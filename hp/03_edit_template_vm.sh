40b522fcb2050ce377859bad4a3f9fc7a6634725
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2014-12-16 02:18:16 +0900

    change default cloud-init user

--- hp/03_edit_template_vm.sh
+++ hp/03_edit_template_vm.sh
@@ -2,7 +2,7 @@ mv -f /etc/cloud/cloud.cfg.rpmnew /etc/cloud/cloud.cfg
 
 sed -i -e 's/^disable_root: 1$/disable_root: 0/' /etc/cloud/cloud.cfg
 sed -i -e 's/^ - set_hostname$/ - [ set_hostname, always]/' /etc/cloud/cloud.cfg
-sed -i -e 's/^    name: cloud-user$/    name: root/' /etc/cloud/cloud.cfg
+sed -i -e 's/^    name: centos$/    name: root/' /etc/cloud/cloud.cfg
 
 rm -rf /var/lib/cloud/*
 rm -rf /etc/ssh/ssh_host_*
